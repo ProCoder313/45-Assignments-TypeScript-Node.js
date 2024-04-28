e2491b78affb57cce8f60dc5bb8013e741d9a77b
Mukhtar Ahmed <neardearsix@gmail.com>
2024-04-29 00:16:50 +0500

    First Commit

---
+++ Q-07 Number Eight/main.js
@@ -0,0 +1,10 @@
+// Q-7.Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results. 
+let addition = 5 + 3;
+console.log('addition = ', addition);
+let subtraction = 10 - 2;
+console.log('subtraction = ', subtraction);
+let multiplication = 2 * 4;
+console.log('multiplication = ', multiplication);
+let division = 16 / 2;
+console.log('division = ', division);
+export {};
---
+++ Q-07 Number Eight/main.ts
@@ -0,0 +1,16 @@
+// Q-7.Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results. 
+
+let addition: number = 5 + 3;
+console.log('addition = ', addition);
+
+let subtraction: number = 10 - 2;
+console.log('subtraction = ', subtraction);
+
+let multiplication: number = 2 * 4;
+console.log('multiplication = ',multiplication);
+
+let division: number = 16 / 2;
+console.log('division = ', division);
+
+
+
