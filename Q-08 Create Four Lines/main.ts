3d648664e2a9d2f27c5fa6e4666fbe56644d9209
Mukhtar Ahmed <neardearsix@gmail.com>
2024-04-30 01:15:07 +0500

    First Commit

---
+++ Q-08 Create Four Lines/main.ts
@@ -0,0 +1,14 @@
+/*Q-8.You should create four lines that look like this:
+
+_____________________________________________
+
+console.log(5 + 3)
+
+_____________________________________________
+
+Your output should simply be four lines with the number 8 appearing once on each line.*/
+
+console.log(`console.log(5 + 3)=`,5+3);
+console.log(`console.log(10 - 2)=`,10-2);
+console.log(`console.log(2 * 4)=`,2*4);
+console.log(`console.log(16 / 2)=`,16/2);
