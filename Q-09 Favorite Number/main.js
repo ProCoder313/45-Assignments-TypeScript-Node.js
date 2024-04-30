0aaa64a801928da68d36660b0749f1e25e81e6d0
Mukhtar Ahmed <neardearsix@gmail.com>
2024-05-01 00:20:02 +0500

    First Commit

---
+++ Q-09 Favorite Number/main.js
@@ -0,0 +1,5 @@
+/* Q-9.Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message. */
+let favoriteNumber = 786;
+let message = `favorite number in message ==> ${favoriteNumber}`;
+console.log(message);
+export {};
---
+++ Q-09 Favorite Number/main.ts
@@ -0,0 +1,8 @@
+ /* Q-9.Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message. */
+
+ let favoriteNumber:number = 786;
+ let message:string = `favorite number in message ==> ${favoriteNumber}`;
+ console.log(message);
+
+
+ 
