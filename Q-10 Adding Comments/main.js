4a9c1498534348196ab1b26dfa3ab30dcf73a0ca
Mukhtar Ahmed <neardearsix@gmail.com>
2024-05-02 00:10:38 +0500

    First Commit

---
+++ Q-10 Adding Comments/main.js
@@ -0,0 +1,15 @@
+/* Q-10.Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+  If you don’t have anything specific to write
+because your programs are too simple at this point, just add your name and the current date at the top of
+ each program file. Then write one sentence
+describing what the program does. */
+//  Name = Mukhtar Ahmed
+//  Date = 10/04/2024
+// HERE WE WILL DO ADD TWO DIFFERENT VALUES.
+// FIRST WE DECLEARE TWO DIFFERENT VALUES IN DIFFERENT VARIABLES.
+var number1 = 20;
+var number2 = 10;
+// NOW WE DECLARE THIRD VARIABLE AND ASSIGNED BOTH VALUES IN NEW VARIABLE.
+var number3 = number1 + number2;
+// FINALLY PRINT.
+console.log(number3);
---
+++ Q-10 Adding Comments/main.ts
@@ -0,0 +1,23 @@
+/* Q-10.Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+  If you don’t have anything specific to write
+because your programs are too simple at this point, just add your name and the current date at the top of
+ each program file. Then write one sentence
+describing what the program does. */
+
+//  Name = Mukhtar Ahmed
+//  Date = 10/04/2024
+
+// HERE WE WILL DO ADD TWO DIFFERENT VALUES.
+
+// FIRST WE DECLEARE TWO DIFFERENT VALUES IN DIFFERENT VARIABLES.
+let number1 = 20;
+let number2 = 10;
+
+// NOW WE DECLARE THIRD VARIABLE AND ASSIGNED BOTH VALUES IN NEW VARIABLE.
+let number3 = number1 + number2;
+
+// FINALLY PRINT.
+console.log(number3);
+
+
+
