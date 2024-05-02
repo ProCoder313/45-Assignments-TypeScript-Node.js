5cee8c4327b6477c5982c72406176f52053f8281
Mukhtar Ahmed <neardearsix@gmail.com>
2024-05-03 00:12:07 +0500

    First Commit

---
+++ Q-11 Names In Array/main.ts
@@ -0,0 +1,12 @@
+//Q-11.Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+// Print In For-Loop Method
+
+let friends_names:string[] = ['Sarfaraz', 'Waqar', 'Abrar', 'Kashif'];
+
+
+for (let i = 0; i < friends_names.length; i++) {console.log(friends_names[i])};
+
+
+
+
